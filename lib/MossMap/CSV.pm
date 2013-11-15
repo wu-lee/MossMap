60fede4d6a154f8292109d9665af8f31c12a76d6
Nick Stokoe <github.wu-lee@noodlefactory.co.uk>
2013-11-15 00:37:34 +0000

    MossMap::CSV - add robustness, more rigorous tests.
    We want to try and deal with various kinds of screwy user input.

--- lib/MossMap/CSV.pm
+++ lib/MossMap/CSV.pm
@@ -16,39 +16,75 @@ my @dinty = map { [split '', $_] } reverse (
     'AFKQV',
 );
 
+# List of regexes indexed by string length
+my @rx_index = do {
+    my $AZ = qr/[A-Z]{2}/;
+    
+    (
+        undef,
+        undef,
+        qr/($AZ)/x,
+        undef,
+        qr/($AZ) \d\d/x,
+        qr/($AZ) (\d) (\d) ([A-Z])/x, # Tetrad, special case, 4 matches
+        qr/($AZ) (\d)(\d) (\d)(\d)/x,
+        undef,
+        qr/($AZ) (\d)(\d)\d (\d)(\d)\d/x,
+        undef,
+        qr/($AZ) (\d)(\d)\d\d (\d)(\d)\d\d/x,
+        undef,
+        qr/($AZ) (\d)(\d)\d\d\d (\d)(\d)\d\d\d/x
+    );
+};
+    
 my %filters = (
     keep_all => sub { shift },
 
     keep_tetrad_attributable => sub {
         my ($row, $trace) = @_;
 
-        # Reject grid-refs with precision less than
-        # PQnnR (tetrad)
-        my $grid_ref = $row->{grid_ref};
-        my $len = length $grid_ref;
-        if ($len < 5) {
-            $trace->("discarding as too coarse: $grid_ref\n");
+        # Ignore undefined gridrefs
+        if (!defined $row->{grid_ref}) {
+            $trace->("discarding as undefined grid ref");
             return;
         }
-            
-        if ($len == 5) {
-            return $row;
+
+        # trim whitespace
+        s/^\s+//sm, s/\s+$//sm for $row->{grid_ref};
+
+        my $grid_ref = $row->{grid_ref};
+        my $len = length $grid_ref;
+
+        # Check for empties
+        if ($len == 0) {
+            croak "empty grid ref $grid_ref";
         }
 
         # Convert other grid-refs to tetrad precision
-        my $rx = (qr/^(..)(.)(.)(.)(.)$/,
-                  undef,
-                  qr/^(..)(.)(.).(.)(.).$/,
-                  undef,
-                  qr/^(..)(.)(.)..(.)(.)..$/,
-                  undef,
-                  qr/^(..)(.)(.)...(.)(.)...$/)[$len-6];
-        croak "invalid grid ref '$grid_ref'"
+        my $rx = $rx_index[$len];
+        croak "invalid grid ref '$grid_ref' has bad length ($len)"
             unless $rx;
+        
+        if ($len < 5) {
+            # We're being strict and checking even GRs we'd discard.
+            croak "invalid grid ref '$grid_ref'"
+                unless $grid_ref =~ /^$rx$/;
+            
+            $trace->("discarding as too coarse: $grid_ref");
+            return;
+        }
+        
+        if ($len == 5) {
+            croak "invalid grid ref '$grid_ref' has 5 characters but is not a tetrad"
+                unless $row->{grid_ref} =~ s/^$rx$/uc $&/ie;
+            return $row;
+        }
 
+        croak "invalid grid ref '$grid_ref'"
+            unless $row->{grid_ref} =~
+                s/^$rx$/join('',uc($1),$2,$4,$dinty[$5>>1][$3>>1])/ie;
 
-        $row->{grid_ref} =~ s/$rx/join('',$1,$2,$4,$dinty[$5>>1][$3>>1])/e;
-        $trace->("converting grid-ref to tetrad: $grid_ref -> $row->{grid_ref}\n");
+        $trace->("converting grid-ref to tetrad: $grid_ref -> $row->{grid_ref}");
         return $row;
     },
 );
@@ -86,7 +122,10 @@ my @heading_map = (
     recorders => sub {
         my $row = shift;
         my $recorder = $row->{recorder};
-        
+
+        return ''
+            unless defined $recorder;
+
         return [ grep { length $_ } 
                  map  { /^\s*(.*?)\s*$/sm }
                  split ';', $recorder        ];
@@ -136,14 +175,15 @@ sub mk_row_iterator {
         $source = $fh;
     }
     elsif (ref $source eq 'ARRAY') {
-        $source = sub { $source->[$ix++] };
+        my $ary = $source;
+        $source = sub { $ary->[$ix++] };
     }
 
     # Create an iterator for coderef or filehandle sources
     if (ref $source eq 'CODE') {
         $iterator = sub {
             my $line = $source->();
-            return ($line && $csv->parse($line));
+            return ($line && $csv->parse($line) && [$csv->fields]);
         };
     }
     elsif (ref $source eq 'GLOB') {
@@ -168,9 +208,9 @@ sub mk_row_iterator {
 # headings, and returned items are taxon, grid_ref, date, and recorder.
 sub mk_filtered_row_iterator {
     my $self = shift;
-    my $trace = $_[1] || $noop;
+    my $trace = $self->{trace_cb} || $noop;
     
-    my $iterator = $self->mk_row_iterator(@_);
+    my $iterator = $self->mk_row_iterator(shift, $trace);
 
     my $headings = $iterator->();
     $headings && @$headings
@@ -184,15 +224,21 @@ sub mk_filtered_row_iterator {
             for @$headings;
 
     # Warn about missing and duplicate headings
-    my @missing = grep { $count{$_} < 1 } qw(taxon gr recorder year);
-    my @dupes = grep { $count{$_} > 1 } qw(taxon gr recorder year month day);
+    {
+        no warnings 'uninitialized'; # some of these $count{}s may be undefined
+        my @missing = grep { $count{$_} < 1 } qw(taxon gr recorder year);
+        
+        @missing 
+            and croak "These mandatory headings are missing even after ".
+                "normalising the input headings: @missing";
 
-    croak "These mandatory headings are missing even after ".
-        "normalising the input headings: @missing"
-            if @missing;
-
-    $trace->("these headings are duplicated after normalising, ".
-                 "so you may not be getting the result you expect: @dupes");
+        my @dupes = grep { $count{$_} > 1 } qw(taxon gr recorder year month day);
+        @dupes
+            and $trace->(
+                "these headings are duplicated after normalising, ".
+                "so you may not be getting the result you expect: @dupes"
+            );
+    }
     
     return sub {
         my %csv_row;
@@ -258,6 +304,14 @@ sub bulk_json {
     } sort keys %index;
  
     return \@list;
+}
+
+sub trace { 
+    my $self = shift;
+    return $self->{trace_cb}
+        unless @_;
+    $self->{trace_cb} = shift;
+    return $self;
 }
 
 no Carp;
