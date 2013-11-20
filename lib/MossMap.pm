59ebe79e8ba4d1271abf26a75a258fc20febf296
Nick Stokoe <github.wu-lee@noodlefactory.co.uk>
2013-11-20 22:47:34 +0000

    lib/MossMap.pm - add a stub top-level module, for docs/packaging purposes

---
+++ lib/MossMap.pm
@@ -0,0 +1,32 @@
+package MossMap;
+use strict;
+use warnings;
+
+=head1 NAME
+
+MossMap - a map of (bryological) observations
+
+=head1 SYNOPSIS
+
+This is a Mojolicious::Lite app with an AngularJS front end.
+
+Run the app like this:
+
+    moss-map.pl
+
+By default it will listen on port 3000.  Alternatively use
+Mojolicious' morbo or hypnotoad, see the Mojolicious documentation for
+more details.
+
+=head1 DESCRIPTION
+
+=head2 CONFIGURATION
+
+There is an example configuration file moss-map.example.json, you can
+copy it into place as moss-map.json and modify as required.
+
+=head1 VERSION
+
+# VERSION
+
+=cut
